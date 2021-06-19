079c0a6e5fe5828e1ee8a6e1bc8a71622addc78b
may <83417030+may-gvg@users.noreply.github.com>
2021-06-19 11:10:52 +0200

    dokończona część zadania

--- main.py
+++ main.py
@@ -1,12 +1,45 @@
+
 from flask import Flask, render_template
+import tmdb_client
+import random
 
 app = Flask(__name__)
 
+from flask import request
+
+def get_filtry():
+    filtry = {"popular": "Popular", "top_rated": "Top Rated", "now_playing": "Now Playing", "upcoming": "Upcoming" }
+    return filtry
+
+
+
 @app.route('/')
 def homepage():
-    movies = [1, 2, 3]
-    return render_template("homepage.html", movies=movies)
+    filtry = get_filtry()
+    selected_list = request.args.get('list_type', "popular")
+    if(selected_list not in get_filtry().keys()):
+        selected_list = 'popular'
+    movies = tmdb_client.get_movies(how_many=8, list_type=selected_list)
+    return render_template("homepage.html", movies=movies, current_list=selected_list, filtry=filtry)
+
+
+@app.route("/movie/<movie_id>")
+def movie_details(movie_id):
+    details = tmdb_client.get_single_movie(movie_id)
+    cast = tmdb_client.get_single_movie_cast(movie_id)
+    movie_images = tmdb_client.get_movie_images(movie_id)
+    selected_backdrop = random.choice(movie_images['backdrops'])
+    return render_template("movie_details.html", movie=details, cast=cast, selected_backdrop=selected_backdrop)
+
+@app.context_processor
+def utility_processor():
+    def tmdb_image_url(path, size):
+        return tmdb_client.get_poster_url(path, size)
+    return {"tmdb_image_url": tmdb_image_url}
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
