2a09428c1b046f31736f97dd89bbc8b9ea6774bb
may <83417030+may-gvg@users.noreply.github.com>
2021-06-23 23:19:47 +0200

    projekt web v6 - ulubione

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request, flash
 import tmdb_client
 import random
+import datetime
+
 
 app = Flask(__name__)
+app.secret_key = b'my-secret'
 
-from flask import request
+
+FAVORITES = set()
+
+
+@app.route("/favorites/add", methods=['POST'])
+def add_to_favorites():
+    data = request.form
+    movie_id = data.get('movie_id')
+    movie_title = data.get('movie_title')
+    if movie_id and movie_title:
+        FAVORITES.add(movie_id)
+        flash(f'Dodano film {movie_title} do ulubionych!')
+    return redirect(url_for('homepage'))
+
+
+@app.route("/favorites/")
+def show_favorites():
+    if FAVORITES:
+        movies = []
+        for movie_id in FAVORITES:
+            movie_details = tmdb_client.get_single_movie(movie_id)
+            movies.append(movie_details)
+    else:
+        movies = []
+    return render_template("homepage.html", movies=movies, filtry=get_filtry())
+
 
 def get_filtry():
     filtry = {"popular": "Popular", "top_rated": "Top Rated", "now_playing": "Now Playing", "upcoming": "Upcoming" }
     return filtry
-
 
 
 @app.route('/')
@@ -31,11 +58,29 @@ def movie_details(movie_id):
     selected_backdrop = random.choice(movie_images['backdrops'])
     return render_template("movie_details.html", movie=details, cast=cast, selected_backdrop=selected_backdrop)
 
+
 @app.context_processor
 def utility_processor():
     def tmdb_image_url(path, size):
         return tmdb_client.get_poster_url(path, size)
     return {"tmdb_image_url": tmdb_image_url}
+
+
+@app.route('/search')
+def search():
+    search_query = request.args.get("q", "")
+    if search_query:
+        movies = tmdb_client.search(search_query=search_query)
+    else:
+        movies = []
+    return render_template("search.html", movies=movies, search_query=search_query)
+
+
+@app.route('/today')
+def today():
+    movies = tmdb_client.get_airing_today()
+    today = datetime.date.today()
+    return render_template("today.html", movies=movies, today=today)
 
 
 if __name__ == '__main__':
