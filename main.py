25697e4a47e72daf49fcc2ba285a88badff874cd
may <83417030+may-gvg@users.noreply.github.com>
2021-06-30 21:32:27 +0200

    zmiany do komentarza

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-
-from flask import Flask, render_template, redirect, url_for, request, flash
+from flask import Flask, render_template, redirect, url_for, request, flash, abort
 import tmdb_client
 import random
 import datetime
@@ -32,6 +31,9 @@ def show_favorites():
             movies.append(movie_details)
     else:
         movies = []
+    for movie in movies:
+        movie['image'] = get_homepage_cover_image(movie)
+
     return render_template("homepage.html", movies=movies, filtry=get_filtry())
 
 
@@ -40,30 +42,39 @@ def get_filtry():
     return filtry
 
 
+def get_homepage_cover_image(movie):
+    return tmdb_client.get_poster_url(movie['poster_path'], "w780")
+    pass
+
+
 @app.route('/')
 def homepage():
     filtry = get_filtry()
     selected_list = request.args.get('list_type', "popular")
-    if(selected_list not in get_filtry().keys()):
+    if selected_list not in get_filtry():
         selected_list = 'popular'
     movies = tmdb_client.get_movies(how_many=8, list_type=selected_list)
+    for movie in movies:
+        movie['image'] = get_homepage_cover_image(movie)
     return render_template("homepage.html", movies=movies, current_list=selected_list, filtry=filtry)
 
 
-@app.route("/movie/<movie_id>")
+@app.route("/movie/<int:movie_id>")
 def movie_details(movie_id):
     details = tmdb_client.get_single_movie(movie_id)
+    if not details.get('success'):
+        abort(404)
     cast = tmdb_client.get_single_movie_cast(movie_id)
     movie_images = tmdb_client.get_movie_images(movie_id)
-    selected_backdrop = random.choice(movie_images['backdrops'])
-    return render_template("movie_details.html", movie=details, cast=cast, selected_backdrop=selected_backdrop)
-
-
-@app.context_processor
-def utility_processor():
-    def tmdb_image_url(path, size):
-        return tmdb_client.get_poster_url(path, size)
-    return {"tmdb_image_url": tmdb_image_url}
+    for actor in cast:
+        actor['image'] = tmdb_client.get_poster_url(actor['profile_path'], "w185")
+    if movie_images.get('backdrops'):
+        selected_backdrop = random.choice(movie_images['backdrops'])
+        image = tmdb_client.get_poster_url(selected_backdrop['file_path'], "w780")
+    else:
+        selected_backdrop = ""
+        image = ""
+    return render_template("movie_details.html", movie=details, cast=cast, selected_backdrop=selected_backdrop, image=image)
 
 
 @app.route('/search')
@@ -73,12 +84,16 @@ def search():
         movies = tmdb_client.search(search_query=search_query)
     else:
         movies = []
+    for movie in movies:
+        movie['image'] = get_homepage_cover_image(movie)
     return render_template("search.html", movies=movies, search_query=search_query)
 
 
 @app.route('/today')
 def today():
     movies = tmdb_client.get_airing_today()
+    for movie in movies:
+        movie['image'] = get_homepage_cover_image(movie)
     today = datetime.date.today()
     return render_template("today.html", movies=movies, today=today)
 
